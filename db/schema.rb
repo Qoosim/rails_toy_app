72cbced5b9665405086ba931c101fff16cde1316
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-02-11 21:31:04 +0100

    Create posts method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_174243) do
+ActiveRecord::Schema.define(version: 2020_02_11_202835) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
